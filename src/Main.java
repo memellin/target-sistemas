e06d2cf13e6769f646c087d0e4d6470b0ad3e11e
Enock <enocksm@outlook.com>
2024-08-29 23:40:44 -0300

    Fibonacci

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,36 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int indice = 13, soma = 0, k = 0;
-        while ( k < indice){
-            k = k +1;
-            soma = soma + k;
+        // 2o Exercício
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        int num = sc.nextInt();
+        boolean fibonacci = Fibonacci(num);
+
+        if (!fibonacci) {
+            System.out.println("O Número "+ num + " não está na sequência de fibonnaci");
+        } else {
+            System.out.println("O Número " + num + " pertence a sequência de fibonacci");
         }
-        System.out.println(soma);
+    }
+    public static boolean Fibonacci(int num) {
+        int n1 = 0, n2 = 1;
+        if (num == 1 || num == 0) {
+            return true;
+        } else{
+            for (int i = 0; i <= num; i++) {
+                int aux = n2;
+                n2 = n2 + n1;
+                n1 = aux;
+                if (n2 == num){
+                    return true;
+                }
+
+            }
+        }
+        return false;
     }
 }
