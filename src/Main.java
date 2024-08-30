c4d5b79dc1248b3694341f98a75ac1f579482ade
Enock <enocksm@outlook.com>
2024-08-29 23:14:21 -0300

    iniciando desafio

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int indice = 13, soma = 0, k = 0;
+        while ( k < indice){
+            k = k +1;
+            soma = soma + k;
+        }
+        System.out.println(soma);
+    }
+}
